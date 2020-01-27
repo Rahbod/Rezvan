d42f3cdd7fd4cab11a68a60d0f122d02878a4ba6
YusefMobasheri <yusef.mobasheri@gmail.com>
2020-01-27 17:09:02 +0330

    Responsive theme

--- private_html/controllers/SiteController.php
+++ private_html/controllers/SiteController.php
@@ -288,79 +288,82 @@ class SiteController extends AuthController
     {
 //        $this->setTheme('default');
 //
-//        $path = 'gs://path/to/your/document.pdf';
-//        $output = 'gs://path/to/store/results/';
-//
-//        # select ocr feature
-//        $feature = (new Feature())
-//            ->setType(Type::DOCUMENT_TEXT_DETECTION);
-//
-//        # set $path (file to OCR) as source
-//        $gcsSource = (new GcsSource())
-//            ->setUri($path);
-//        # supported mime_types are: 'application/pdf' and 'image/tiff'
-//        $mimeType = 'application/pdf';
-//        $inputConfig = (new InputConfig())
+        $path = 'gs://path/to/your/document.pdf';
+        $path = alias('@webroot/uploads/pdf/Request file - 370.pdf');
+        $output = 'gs://path/to/store/results/';
+        $output = alias('@webroot/uploads/pdf/Request file - 370.pdf.txt');
+
+        # select ocr feature
+        $feature = (new Feature())
+            ->setType(Type::DOCUMENT_TEXT_DETECTION);
+
+        # set $path (file to OCR) as source
+        $gcsSource = (new GcsSource())
+            ->setUri($path);
+        # supported mime_types are: 'application/pdf' and 'image/tiff'
+        $mimeType = 'application/pdf';
+        $inputConfig = (new InputConfig())
+            ->setContent(file_get_contents($path))
 //            ->setGcsSource($gcsSource)
-//            ->setMimeType($mimeType);
-//
-//        # set $output as destination
-//        $gcsDestination = (new GcsDestination())
-//            ->setUri($output);
-//        # how many pages should be grouped into each json output file.
-//        $batchSize = 2;
-//        $outputConfig = (new OutputConfig())
-//            ->setGcsDestination($gcsDestination)
-//            ->setBatchSize($batchSize);
-//
-//        # prepare request using configs set above
-//        $request = (new AsyncAnnotateFileRequest())
-//            ->setFeatures([$feature])
-//            ->setInputConfig($inputConfig)
-//            ->setOutputConfig($outputConfig);
-//        $requests = [$request];
-//
-//        # make request
-//        $imageAnnotator = new ImageAnnotatorClient();
-//        $operation = $imageAnnotator->asyncBatchAnnotateFiles($requests);
-//        print('Waiting for operation to finish.' . PHP_EOL);
-//        $operation->pollUntilComplete();
-//
-//        # once the request has completed and the output has been
-//        # written to GCS, we can list all the output files.
-//        preg_match('/^gs:\/\/([a-zA-Z0-9\._\-]+)\/?(\S+)?$/', $output, $match);
-//        $bucketName = $match[1];
-//        $prefix = isset($match[2]) ? $match[2] : '';
-//
-//        $storage = new StorageClient();
-//        $bucket = $storage->bucket($bucketName);
-//        $options = ['prefix' => $prefix];
-//        $objects = $bucket->objects($options);
-//
-//        # save first object for sample below
-//        $objects->next();
-//        $firstObject = $objects->current();
-//
-//        # list objects with the given prefix.
-//        print('Output files:' . PHP_EOL);
-//        foreach ($objects as $object) {
-//            print($object->name() . PHP_EOL);
-//        }
-//
-//        # process the first output file from GCS.
-//        # since we specified batch_size=2, the first response contains
-//        # the first two pages of the input file.
-//        $jsonString = $firstObject->downloadAsString();
-//        $firstBatch = new AnnotateFileResponse();
-//        $firstBatch->mergeFromJsonString($jsonString);
-//
-//        # get annotation and print text
-//        foreach ($firstBatch->getResponses() as $response) {
-//            $annotation = $response->getFullTextAnnotation();
-//            print($annotation->getText());
-//        }
-//
-//        $imageAnnotator->close();
+            ->setMimeType($mimeType);
+
+        # set $output as destination
+        $gcsDestination = (new GcsDestination())
+            ->setUri($output);
+        # how many pages should be grouped into each json output file.
+        $batchSize = 2;
+        $outputConfig = (new OutputConfig())
+            ->setGcsDestination($gcsDestination)
+            ->setBatchSize($batchSize);
+
+        # prepare request using configs set above
+        $request = (new AsyncAnnotateFileRequest())
+            ->setFeatures([$feature])
+            ->setInputConfig($inputConfig)
+            ->setOutputConfig($outputConfig);
+        $requests = [$request];
+
+        # make request
+        $imageAnnotator = new ImageAnnotatorClient();
+        $operation = $imageAnnotator->asyncBatchAnnotateFiles($requests);
+        print('Waiting for operation to finish.' . PHP_EOL);
+        $operation->pollUntilComplete();
+
+        # once the request has completed and the output has been
+        # written to GCS, we can list all the output files.
+        preg_match('/^gs:\/\/([a-zA-Z0-9\._\-]+)\/?(\S+)?$/', $output, $match);
+        $bucketName = $match[1];
+        $prefix = isset($match[2]) ? $match[2] : '';
+
+        $storage = new StorageClient();
+        $bucket = $storage->bucket($bucketName);
+        $options = ['prefix' => $prefix];
+        $objects = $bucket->objects($options);
+
+        # save first object for sample below
+        $objects->next();
+        $firstObject = $objects->current();
+
+        # list objects with the given prefix.
+        print('Output files:' . PHP_EOL);
+        foreach ($objects as $object) {
+            print($object->name() . PHP_EOL);
+        }
+
+        # process the first output file from GCS.
+        # since we specified batch_size=2, the first response contains
+        # the first two pages of the input file.
+        $jsonString = $firstObject->downloadAsString();
+        $firstBatch = new AnnotateFileResponse();
+        $firstBatch->mergeFromJsonString($jsonString);
+
+        # get annotation and print text
+        foreach ($firstBatch->getResponses() as $response) {
+            $annotation = $response->getFullTextAnnotation();
+            print($annotation->getText());
+        }
+
+        $imageAnnotator->close();
     }
 
 //    public function actionClearFiles()
--- public_html/themes/frontend/assets/js/custom.js
+++ public_html/themes/frontend/assets/js/custom.js
@@ -1,26 +1,54 @@
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 800) {
-        $('.fade').fadeIn();
-    } else {
-        $('.fade').fadeOut();
-    }
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 800) {
+    $('.fade').fadeIn();
+  } else {
+    $('.fade').fadeOut();
+  }
+
+  if (y > 100) {
+    $('.site-header').addClass('fixed-menu');
+  } else {
+    $('.site-header').removeClass('fixed-menu');
+  }
 });
 
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 100) {
-        $('.site-header').addClass('fixed-menu');
-    } else {
-        $('.site-header').removeClass('fixed-menu');
-    }
+$(document).ready(function() {
+	var newwidth = $('#way');
+    //replace the width
+	$('#way').css("min-width", + $(".infography").outerWidth());
+	newwidth.css({'min-width': + $(".infography").outerWidth()});
+	newwidth.show();
+
+	var newheight = $('.main-info');
+    //replace the width
+	$('.main-info').css("min-height", + $(".infography").outerWidth());
+	newheight.css({'min-height': + $(".infography").outerWidth()});
+	newheight.show();
+
+	var newheight = $('.way-line');
+    //replace the width
+	$('.way-line').css("min-height", + $(".infography").outerWidth());
+	newheight.css({'min-height': + $(".infography").outerWidth()});
+	newheight.show();
 });
 
-$(function () {
-    $("body").on('click', 'a.lazy-scroll', function (e) {
-        e.preventDefault();
-        $("html, body").animate({
-          scrollTop: $($(this).attr("href")).offset().top
-        }, 2000);
-    });
+$(window).resize(function() {
+	var newwidth = $('#way');
+    //replace the width
+	$('#way').css("min-width", + $(".infography").outerWidth());
+	newwidth.css({'min-width': + $(".infography").outerWidth()});
+	newwidth.show();
+
+	var newheight = $('.main-info');
+    //replace the width
+	$('.main-info').css("min-height", + $(".infography").outerWidth());
+	newheight.css({'min-height': + $(".infography").outerWidth()});
+	newheight.show();
+
+	var newheight = $('.way-line');
+    //replace the width
+	$('.way-line').css("min-height", + $(".infography").outerWidth());
+	newheight.css({'min-height': + $(".infography").outerWidth()});
+	newheight.show();
 });
