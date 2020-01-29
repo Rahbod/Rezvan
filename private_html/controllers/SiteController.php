68884ca641bf456dade42f3f1c940fab72f3fb19
YusefMobasheri <yusef.mobasheri@gmail.com>
2020-01-29 14:15:42 +0330

    add device detect package

--- private_html/controllers/SiteController.php
+++ private_html/controllers/SiteController.php
@@ -6,36 +6,19 @@ use app\components\AuthController;
 use app\components\customWidgets\CustomCaptchaAction;
 use app\components\MultiLangActiveRecord;
 use app\components\Setting;
-use app\models\Block;
 use app\models\ContactForm;
 use app\models\Message;
 use app\models\projects\Apartment;
 use app\models\projects\ApartmentSearch;
 use app\models\projects\Investment;
 use app\models\projects\OtherConstruction;
-use app\models\Request;
 use app\models\Service;
 use app\models\Slide;
-use app\models\Unit;
-use kartik\mpdf\Pdf;
 use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
-use yii\helpers\Html;
-use yii\web\HttpException;
 use yii\web\Response;
 use yii\widgets\ActiveForm;
-
-use Google\Cloud\Storage\StorageClient;
-use Google\Cloud\Vision\V1\AnnotateFileResponse;
-use Google\Cloud\Vision\V1\AsyncAnnotateFileRequest;
-use Google\Cloud\Vision\V1\Feature;
-use Google\Cloud\Vision\V1\Feature\Type;
-use Google\Cloud\Vision\V1\GcsDestination;
-use Google\Cloud\Vision\V1\GcsSource;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-use Google\Cloud\Vision\V1\InputConfig;
-use Google\Cloud\Vision\V1\OutputConfig;
 
 //use Symfony\Component\EventDispatcher\Tests\Service;
 
@@ -284,87 +267,87 @@ class SiteController extends AuthController
         return $this->render('info');
     }
 
-    public function actionOcr()
-    {
-//        $this->setTheme('default');
+//    public function actionOcr()
+//    {
+////        $this->setTheme('default');
+////
+//        $path = 'gs://path/to/your/document.pdf';
+//        $path = alias('@webroot/uploads/pdf/Request file - 370.pdf');
+//        $output = 'gs://path/to/store/results/';
+//        $output = alias('@webroot/uploads/pdf/Request file - 370.pdf.txt');
 //
-        $path = 'gs://path/to/your/document.pdf';
-        $path = alias('@webroot/uploads/pdf/Request file - 370.pdf');
-        $output = 'gs://path/to/store/results/';
-        $output = alias('@webroot/uploads/pdf/Request file - 370.pdf.txt');
-
-        # select ocr feature
-        $feature = (new Feature())
-            ->setType(Type::DOCUMENT_TEXT_DETECTION);
-
-        # set $path (file to OCR) as source
-        $gcsSource = (new GcsSource())
-            ->setUri($path);
-        # supported mime_types are: 'application/pdf' and 'image/tiff'
-        $mimeType = 'application/pdf';
-        $inputConfig = (new InputConfig())
-            ->setContent(file_get_contents($path))
-//            ->setGcsSource($gcsSource)
-            ->setMimeType($mimeType);
-
-        # set $output as destination
-        $gcsDestination = (new GcsDestination())
-            ->setUri($output);
-        # how many pages should be grouped into each json output file.
-        $batchSize = 2;
-        $outputConfig = (new OutputConfig())
-            ->setGcsDestination($gcsDestination)
-            ->setBatchSize($batchSize);
-
-        # prepare request using configs set above
-        $request = (new AsyncAnnotateFileRequest())
-            ->setFeatures([$feature])
-            ->setInputConfig($inputConfig)
-            ->setOutputConfig($outputConfig);
-        $requests = [$request];
-
-        # make request
-        $imageAnnotator = new ImageAnnotatorClient();
-        $operation = $imageAnnotator->asyncBatchAnnotateFiles($requests);
-        print('Waiting for operation to finish.' . PHP_EOL);
-        $operation->pollUntilComplete();
-
-        # once the request has completed and the output has been
-        # written to GCS, we can list all the output files.
-        preg_match('/^gs:\/\/([a-zA-Z0-9\._\-]+)\/?(\S+)?$/', $output, $match);
-        $bucketName = $match[1];
-        $prefix = isset($match[2]) ? $match[2] : '';
-
-        $storage = new StorageClient();
-        $bucket = $storage->bucket($bucketName);
-        $options = ['prefix' => $prefix];
-        $objects = $bucket->objects($options);
-
-        # save first object for sample below
-        $objects->next();
-        $firstObject = $objects->current();
-
-        # list objects with the given prefix.
-        print('Output files:' . PHP_EOL);
-        foreach ($objects as $object) {
-            print($object->name() . PHP_EOL);
-        }
-
-        # process the first output file from GCS.
-        # since we specified batch_size=2, the first response contains
-        # the first two pages of the input file.
-        $jsonString = $firstObject->downloadAsString();
-        $firstBatch = new AnnotateFileResponse();
-        $firstBatch->mergeFromJsonString($jsonString);
-
-        # get annotation and print text
-        foreach ($firstBatch->getResponses() as $response) {
-            $annotation = $response->getFullTextAnnotation();
-            print($annotation->getText());
-        }
-
-        $imageAnnotator->close();
-    }
+//        # select ocr feature
+//        $feature = (new Feature())
+//            ->setType(Type::DOCUMENT_TEXT_DETECTION);
+//
+//        # set $path (file to OCR) as source
+//        $gcsSource = (new GcsSource())
+//            ->setUri($path);
+//        # supported mime_types are: 'application/pdf' and 'image/tiff'
+//        $mimeType = 'application/pdf';
+//        $inputConfig = (new InputConfig())
+//            ->setContent(file_get_contents($path))
+////            ->setGcsSource($gcsSource)
+//            ->setMimeType($mimeType);
+//
+//        # set $output as destination
+//        $gcsDestination = (new GcsDestination())
+//            ->setUri($output);
+//        # how many pages should be grouped into each json output file.
+//        $batchSize = 2;
+//        $outputConfig = (new OutputConfig())
+//            ->setGcsDestination($gcsDestination)
+//            ->setBatchSize($batchSize);
+//
+//        # prepare request using configs set above
+//        $request = (new AsyncAnnotateFileRequest())
+//            ->setFeatures([$feature])
+//            ->setInputConfig($inputConfig)
+//            ->setOutputConfig($outputConfig);
+//        $requests = [$request];
+//
+//        # make request
+//        $imageAnnotator = new ImageAnnotatorClient();
+//        $operation = $imageAnnotator->asyncBatchAnnotateFiles($requests);
+//        print('Waiting for operation to finish.' . PHP_EOL);
+//        $operation->pollUntilComplete();
+//
+//        # once the request has completed and the output has been
+//        # written to GCS, we can list all the output files.
+//        preg_match('/^gs:\/\/([a-zA-Z0-9\._\-]+)\/?(\S+)?$/', $output, $match);
+//        $bucketName = $match[1];
+//        $prefix = isset($match[2]) ? $match[2] : '';
+//
+//        $storage = new StorageClient();
+//        $bucket = $storage->bucket($bucketName);
+//        $options = ['prefix' => $prefix];
+//        $objects = $bucket->objects($options);
+//
+//        # save first object for sample below
+//        $objects->next();
+//        $firstObject = $objects->current();
+//
+//        # list objects with the given prefix.
+//        print('Output files:' . PHP_EOL);
+//        foreach ($objects as $object) {
+//            print($object->name() . PHP_EOL);
+//        }
+//
+//        # process the first output file from GCS.
+//        # since we specified batch_size=2, the first response contains
+//        # the first two pages of the input file.
+//        $jsonString = $firstObject->downloadAsString();
+//        $firstBatch = new AnnotateFileResponse();
+//        $firstBatch->mergeFromJsonString($jsonString);
+//
+//        # get annotation and print text
+//        foreach ($firstBatch->getResponses() as $response) {
+//            $annotation = $response->getFullTextAnnotation();
+//            print($annotation->getText());
+//        }
+//
+//        $imageAnnotator->close();
+//    }
 
 //    public function actionClearFiles()
 //    {
@@ -432,4 +415,10 @@ class SiteController extends AuthController
 //
 //        dd(Unit::find()->count(), $valid, $invalid);
 //    }
+
+    public function actionTest()
+    {
+        dd(app()->devicedetect);
+    }
+
 }
